948d4fb0619480f1c843180154ab6585bd767981
Ethan Diosana <ethandiosana1@gmail.com>
2024-03-04 20:24:06 -0800

    Got search function ready for endpoint.

--- admin-panel/src/Components/AdminPage/UserList/UserList.jsx
+++ admin-panel/src/Components/AdminPage/UserList/UserList.jsx
@@ -1,61 +1,111 @@
-import testUsers from '../../../data/testUsers.json' 
+import testUsers from '../../../data/testUsers.json'
 import { UserListItem } from './UserListItem'
 import './UserList.css'
 import { UserListSearchBar } from './UserListSearchBar'
 import { useState } from 'react'
 
 /** Displays a list of users.
+ * Includes a search bar.
  *
  * @param {{}} props
  * @returns
  */
-export function UserList({ editUserButtonHandler }) {
+export function UserList({ editUserButtonHandler, setUserList }) {
   /** Contains the users list. */
-  const [usersList, setUsersList] = useState(null);
+  const [users, setUsers] = useState(null);
 
-  /** The number of users per page. */
+  // Search bar params.
+  const [searchTerm, setSearchTerm] = useState('');
+  const [fieldToSearch, setFieldToSearch] = useState('');
   const [usersPerPage, setUsersPerPage] = useState(20);
 
   /** Retrieves all users from the db. */
   function getAllUsers(usersPerPage) {
     try {
-      if (!usersList) {
-        setUsersList(testUsers.testUsers);
-        return usersList;
+      if (!users) {
+
+        const dbResponse = testUsers.testUsers;
+
+        setUsers(dbResponse);
+        return users;
       }
     } catch (error) {
       console.log(error)
     }
   }
-  
+
+  /** Get all of the users. */
+  getAllUsers(usersPerPage);
+
   /** Handles the search bar being clicked.
    * 
    * @param {string} fieldName The name of the field to search. 
    * @param {string} fieldValue The value of the field to search.
    * @param {number} usersPerPage The number of users per page.
    */
-  function searchButtonHandler(fieldName, fieldValue, usersPerPage) {
-    if(!fieldName) {
-      throw new Error(`!fieldName`);
-    }
-    if(!fieldValue) {
-      throw new Error(`!fieldValue`);
-    }
-    if(!usersPerPage) {
-      throw new Error(`!usersPerPage`);
+  async function searchButtonHandler(fieldName, fieldValue, usersPerPage, setUserList) {
+    try {
+      let endpoint; // The API endpoint to call.
+      let response; // The response from the server.
+      let result; // The json result from the server.
+      let params; // The params for the search.
+
+      if (!fieldName) {
+        throw new Error(`!fieldName`);
+      }
+      if (!fieldValue) {
+        throw new Error(`!fieldValue`);
+      }
+      if (!usersPerPage) {
+        throw new Error(`!usersPerPage`);
+      }
+
+      // TODO: replace this with the real endpoint.
+      endpoint = 'endpoint';
+
+      // Set the params
+      params = {
+        'fieldName': fieldName,
+        'fieldValue': fieldValue,
+        'usersPerPage': usersPerPage
+      }
+
+      // Get the response from the server.
+      response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error!`)
+      }
+
+      // Convert to json.
+      result = await response.json();
+
+      // Throw error if response is not array.
+      if (!result) {
+        throw new Error(`result is ${result}`);
+      }
+
+      console.trace(result);
+
+      // Set the current user list.
+      setUserList(response);
+
+    } catch (error) {
+      console.log(error);
     }
 
-    // TODO: Ensure that all of these html fields are required.
-
-    // TODO: Add search bar handling to the Trello.
-
-    // TODO: Download TODO highlight
   }
 
   return (
     <>
       <UserListSearchBar
-      searchButtonHandler={searchButtonHandler} />
+        searchButtonHandler={searchButtonHandler} />
       <table className='user-list'>
         <thead>
           <tr>
@@ -66,16 +116,16 @@ export function UserList({ editUserButtonHandler }) {
           </tr>
         </thead>
         <tbody>
-          { usersList ? 
-            
-            usersList.map((user, index) => {
+          {users ?
 
-            return <UserListItem
-              key={index}
-              user={user}
-              editUserButtonHandler={editUserButtonHandler}
-            />
-          }) : <tr>No users to display.</tr>}
+            users.map((user, index) => {
+
+              return <UserListItem
+                key={index}
+                user={user}
+                editUserButtonHandler={editUserButtonHandler}
+              />
+            }) : <tr>No users to display.</tr>}
         </tbody>
       </table>
     </>
